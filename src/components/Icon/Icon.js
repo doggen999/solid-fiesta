cae7a387a65fbada078b4a59790c645293f0e514
Natan Williams <natan.williams123@gmail.com>
2022-11-18 21:34:34 +0100

    Add / Icon (svg) component

---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import classnames from "classnames";
+
+import iconDefinitions from "../../assets/icons/iconDefinitions";
+
+import styles from "./Icon.module.scss";
+
+export const Icon = ({ type, clickable }) => {
+  return type ? (
+    <svg className={classnames(styles.icon, { [styles.clickable]: clickable })}>
+      <use href={`#${type}`} />
+      {iconDefinitions}
+    </svg>
+  ) : (
+    <span className={styles.icon}>☒</span>
+  );
+};
+
+Icon.propTypes = {
+  type: PropTypes.string.isRequired,
+  clickable: PropTypes.func,
+};
+
+export default Icon;
---
+++ src/components/Icon/Icon.stories.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Icon from "./index";
+
+export default {
+  title: "Components/Icon",
+  component: Icon,
+};
+export const Primary = () => (
+  <>
+    <Icon type="star_empty" />
+    <br />
+    <Icon type="star_filled" />
+    <br />
+    <Icon type="website" />
+    <br />
+    <Icon type="address" />
+    <br />
+    <Icon type="chevron_left" clickable />
+    <br />
+    <Icon type="chevron_down" clickable />
+    <br />
+    <Icon type="chevron_right" clickable />
+    <br />
+    <Icon type="heart" clickable />
+    <br />
+    <Icon type="phone" />
+    <br />
+    <Icon type="settings" clickable />
+    <br />
+    <Icon type="time" />
+    <br />
+    <Icon />
+  </>
+);
+
+export const ClickableIcon = () => <Icon type="heart" clickable />;
+
+export const MissingIconType = () => <Icon />;
