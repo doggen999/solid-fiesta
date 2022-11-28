d1e83875b9868f581af533d8b94214e02eac9803
Natan Williams <natan.williams123@gmail.com>
2022-11-28 21:42:51 +0100

    Update / Replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import App from "./app";
 
 ReactDom.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("app")
 );
